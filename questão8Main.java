cbb7952145bb199876b9735eadd054e14540809b
Thomas-MeC <61051354+Thomas-MeC@users.noreply.github.com>
2021-08-10 21:06:35 -0300

    Add files via upload

---
+++ questão8Main.java
@@ -0,0 +1,14 @@
+
+public class questão8Main {
+	
+	public static void main (String[] args) 
+	{
+		
+		System.out.println("Gestação do Elefante Indiano: \nDias: 624\nHoras: " + conversãoUnidadeTempo.diaParaHoras(624)+ "\nMinutos: " + conversãoUnidadeTempo.diaParaMinutos(624) + "\nSegundos: " + conversãoUnidadeTempo.diaParaSegundos(624));
+		
+		
+	}
+	
+
+
+}
